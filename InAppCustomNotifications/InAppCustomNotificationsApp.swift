b4ba0e219da0ae8808d270e339edbc7dffbad033
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-07 11:43:55 +0300

    Initial Commit

---
+++ InAppCustomNotifications/InAppCustomNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppCustomNotificationsApp.swift
+//  InAppCustomNotifications
+//
+//  Created by Yunus Emre Taşdemir on 7.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InAppCustomNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
